467611732b0778294b9ad2e002dffb76cb27a03f
kurtdashielpacaigue <kurtdashielpacaigue@gmail.com>
2025-08-17 22:48:43 +0800

    Create code_challenge1.py

---
+++ code_challenge1.py
@@ -0,0 +1,2 @@
+Name= input ("type your name:")
+print("\t\t\t\t*\n\t\t\t*\t\t*\n\t\t*\t\t\t\t*\n\t*\t\thi\t\t\t\t*\n*\t\t\t",Name,"\t\t\t\t\t*\n\t*\t\t\t\t\t\t*\n\t\t*\t\t\t\t*\n\t\t\t*\t\t*\n\t\t\t\t*")
