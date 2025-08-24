5b08a5eb1d150e01ef0aa75ac28c03e338c409af
kurtdashielpacaigue <kurtdashielpacaigue@gmail.com>
2025-08-24 12:30:00 +0800

    Create Activity7.py

---
+++ Activity7.py
@@ -0,0 +1,14 @@
+K= 5                                  
+print("the value of a is",K )
+
+
+K +=5
+
+print("the value of a is ",K )                                                                                                                          
+K+=3
+K+=13
+K+=4
+K*=2
+
+
+print("the value of a is",K )
