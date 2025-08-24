86d8872ad5c90392abc238c07009c8fe5595d654
kurtdashielpacaigue <kurtdashielpacaigue@gmail.com>
2025-08-24 12:48:09 +0800

    Create Activity9.py

---
+++ Activity9.py
@@ -0,0 +1,15 @@
+a= 3
+b= 2
+
+c= 6
+c= 5
+
+username='balls'
+password = 'balls123'
+
+print("USER LOGIN")
+print( (username == 'balls') and ( password == 'balls123'))
+
+#print( (a>b) and (c < d )
+#print( (a < b) or (c< d)
+#print( not((a<b) or (c<d)) )
