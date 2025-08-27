89d6d6addcb5d902874d461a3c273d70087fb5de
kurtdashielpacaigue <kurtdashielpacaigue@gmail.com>
2025-08-27 13:57:43 +0800

    Create Activity10.py

---
+++ Activity10.py
@@ -0,0 +1,10 @@
+username= 'kurt'
+pword= 'tachyon'
+
+u== input("USERNAME -->")
+p== input("PASSWORD -->")
+
+if (u ==username) and (p==pword) :
+    print("Access granted")
+else:
+    print("Access Denied")
