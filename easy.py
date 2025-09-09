767cd52a8897e4421d79eab7832e3b921738cd18
kurtdashielpacaigue <kurtdashielpacaigue@gmail.com>
2025-09-09 13:13:36 +0800

    Create easy.py

---
+++ easy.py
@@ -0,0 +1,23 @@
+messages=""
+person1= input("enter variable:")
+messages += person1
+person2= input("enter variable:")
+messages+=person2
+person3= input("enter variable:")
+messages+=person3
+person4= input("enter variable:")
+messages+=person4
+person5= input("enter variable:")
+messages+=person5
+person6= input("enter variable:")
+messages+=person6
+person7= input("enter variable:")
+messages+=person7
+person8= input("enter variable:")
+messages+=person8
+person9= input("enter variable:")
+messages+=person9
+person10= input("enter variable:")
+messages+=person10
+
+print(messages)
