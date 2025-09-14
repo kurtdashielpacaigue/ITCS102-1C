fc97edba757eb0c753f026d74f57068319a850a1
kurtdashielpacaigue <kurtdashielpacaigue@gmail.com>
2025-09-14 16:59:33 +0800

    Update training.py

--- training.py
+++ training.py
@@ -1,187 +1,140 @@
 print("idk wut")
-print("1.mango")
-print("2.sixseven")
-print("3.balls")
-meymey=input("choose your character")
+print("1. mango")
+print("2. sixseven")
+print("3. balls")
+meymey = input("Choose your character: ")
 
-if meymey =="1" :
-    man ="mangos"
-    print("you selected", man)
-print("1.genz")
-print("2.gen4")
-print("3.vengen")
-gen=input("choose your gens")
+# ============ MANGO ============
+if meymey == "1":
+    print("You selected mango.")
+    print("1. genz")
+    print("2. gen4")
+    print("3. vengen")
+    gen = input("Choose your gens: ")
 
-if gen =="1":
-    mey="genz"
-    print("you selected", mey)
+    if gen == "1":
+        print("You selected genz.")
+        print("1. 2010")
+        print("2. 2000")
+        dec = input("Choose your decade: ")
+        if dec == "1":
+            print("You should watch shikanokoko koshtantan")
+        elif dec == "2":
+            print("You should watch zombie land saga")
+        else:
+            print("yourself")
 
-print("1.2010")
-print("2.2000")
-dec=input("choose your decade")
+    elif gen == "2":
+        print("You selected vengen.")
+        print("1. 2010")
+        print("2. 2000")
+        dec = input("Choose your decade: ")
+        if dec == "1":
+            print("You should watch quintessntial quintuplets")
+        elif dec == "2":
+            print("Attack on Titan")
+        else:
+            print("yourself")
 
-if dec =="1":
-   print("you should watch shikanokoko koshtantan")
+    elif gen == "3":
+        print("You selected gen4.")
+        print("1. 2010")
+        print("2. 2000")
+        dec = input("Choose your decade: ")
+        if dec == "1":
+            print("Sakamoto Araws")
+        elif dec == "2":
+            print("Slam Dunk")
+        else:
+            print("yourself")
 
-elif dec == "2":
-     print("you should watch zombie land saga")
+# ============ SIXSEVEN ============
+elif meymey == "2":
+    print("You selected sixseven.")
+    print("1. genz")
+    print("2. gen4")
+    print("3. vengen")
+    gen = input("Choose your gens: ")
 
-else:
-    print("kill yourself")
-  
-elif gen =="2":
-    mey22="vengen"
-    print("you selected", mey2)
+    if gen == "1":
+        print("You selected genz.")
+        print("1. 2010")
+        print("2. 2000")
+        dec = input("Choose your decade: ")
+        if dec == "1":
+            print("Mayo Chiki")
+        elif dec == "2":
+            print("Overflow")
+        else:
+            print("yourself")
 
-print("1.2010")
-print("2.2000")
-dec=input("choose your decade")
+    elif gen == "2":
+        print("You selected gen4.")
+        print("1. 2010")
+        print("2. 2000")
+        dec = input("Choose your decade: ")
+        if dec == "1":
+            print("Sub to PewDiePie")
+        elif dec == "2":
+            print("Just watch Filthy Frank or something")
+        else:
+            print("yourself")
 
-if dec =="1":
-   print("you should watch quintessntial quintuplets")
+    elif gen == "3":
+        print("You selected gen4.")
+        print("1. 2010")
+        print("2. 2000")
+        dec = input("Choose your decade: ")
+        if dec == "1":
+            print("Hajime no Ippo")
+        elif dec == "2":
+            print("Why are you born")
+        else:
+            print("yourself")
 
-elif dec == "2":
-     print("attack on titans")
+# ============ BALLS ============
+elif meymey == "3":
+    print("You selected balls.")
+    print("1. genz")
+    print("2. gen4")
+    print("3. vengen")
+    gen = input("Choose your gens: ")
 
-else:
-    print("kill yourself")
+    if gen == "1":
+        print("You selected genz.")
+        print("1. 2010")
+        print("2. 2000")
+        dec = input("Choose your decade: ")
+        if dec == "1":
+            print("Hunter x Hunter")
+        elif dec == "2":
+            print("your mom")
+        else:
+            print("yourself")
 
-elif gen =="3":
-    mey22="gen4"
-    print("you selected", mey22)
+    elif gen == "2":
+        print("You selected gen4.")
+        print("1. 2010")
+        print("2. 2000")
+        dec = input("Choose your decade: ")
+        if dec == "1":
+            print("Karakai Jouzu no Takagi-san")
+        elif dec == "2":
+            print("Dragon Ball Z")
+        else:
+            print("yourself")
 
-print("1.2010")
-print("2.2000")
-dec=input("choose your decade")
-
-if dec =="1":
-   print("sakamoto araws")
-
-elif dec == "2":
-     print("slam dunk")
-
-else:
-    print("kill yourself")
-  
-
-if meymey =="2" :
-    six ="sixseven"
-    print("you selected", six)
-print("1.genz")
-print("2.gen4")
-print("3.vengen")
-gen=input("choose your gens")
-
-if gen =="1":
-    mey="genz"
-    print("you selected", mey)
-
-print("1.2010")
-print("2.2000")
-dec=input("choose your decade")
-
-if dec =="1":
-   print("mayo chiki")
-
-elif dec == "2":
-     print("overflow")
-
-else:
-    print("kill yourself")
-  
-elif gen =="2":
-    mey22="gen4"
-    print("you selected", mey2)
-
-print("1.2010")
-print("2.2000")
-dec=input("choose your decade")
-
-if dec =="1":
-   print("subtopewdiepie")
-
-elif dec == "2":
-     print("just watch filthy frank or something")
-
-else:
-    print("kill yourself")
-
-elif gen =="3":
-    mey22="gen4"
-    print("you selected", mey2)
-
-print("1.2010")
-print("2.2000")
-dec=input("choose your decade")
-
-if dec =="1":
-   print("hajime no ippo")
-
-elif dec == "2":
-     print("why are you born")
-
-else:
-    print("kill yourself")
-  
-  
-if meymey =="3" :
-    ball ="balls"
-    print("you selected", ball)
-print("1.genz")
-print("2.gen4")
-print("3.vengen")
-gen=input("choose your gens")
-
-if gen =="1":
-    mey="genz"
-    print("you selected", mey)
-
-print("1.2010")
-print("2.2000")
-dec=input("choose your decade")
-
-if dec =="1":
-   print("hunter eks hunter")
-
-elif dec == "2":
-     print("your mom")
+    elif gen == "3":
+        print("You selected gen4.")
+        print("1. 2010")
+        print("2. 2000")
+        dec = input("Choose your decade: ")
+        if dec == "1":
+            print("Bohemian Rhapsody")
+        elif dec == "2":
+            print("Slam Dunk")
+        else:
+            print("JoJo Bizarre Adventure")
 
 else:
-    print("kill yourself")
-  
-elif gen =="2":
-    mey22="gen4"
-    print("you selected", mey2)
-
-print("1.2010")
-print("2.2000")
-dec=input("choose your decade")
-
-if dec =="1":
-   print("karakai jouzu no takagi san")
-
-elif dec == "2":
-     print("dragonballz")
-
-else:
-    print("kill yourself")
-
-elif gen =="3":
-    mey22="gen4"
-    print("you selected", mey2)
-
-print("1.2010")
-print("2.2000")
-dec=input("choose your decade")
-
-if dec =="1":
-   print("bohemian rhapsody")
-
-elif dec == "2":
-     print("slam dunk")
-
-else:
-    print("jojo bizzare adventure")
-  
-  
-  
+    print("Invalid character selection.")
