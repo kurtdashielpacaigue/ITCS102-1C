19adc759874ff41014b1c87c09e877e5604fc519
kurtdashielpacaigue <kurtdashielpacaigue@gmail.com>
2025-09-14 15:10:15 +0800

    Create training.py

---
+++ training.py
@@ -0,0 +1,187 @@
+print("idk wut")
+print("1.mango")
+print("2.sixseven")
+print("3.balls")
+meymey=input("choose your character")
+
+if meymey =="1" :
+    man ="mangos"
+    print("you selected", man)
+print("1.genz")
+print("2.gen4")
+print("3.vengen")
+gen=input("choose your gens")
+
+if gen =="1":
+    mey="genz"
+    print("you selected", mey)
+
+print("1.2010")
+print("2.2000")
+dec=input("choose your decade")
+
+if dec =="1":
+   print("you should watch shikanokoko koshtantan")
+
+elif dec == "2":
+     print("you should watch zombie land saga")
+
+else:
+    print("kill yourself")
+  
+elif gen =="2":
+    mey22="vengen"
+    print("you selected", mey2)
+
+print("1.2010")
+print("2.2000")
+dec=input("choose your decade")
+
+if dec =="1":
+   print("you should watch quintessntial quintuplets")
+
+elif dec == "2":
+     print("attack on titans")
+
+else:
+    print("kill yourself")
+
+elif gen =="3":
+    mey22="gen4"
+    print("you selected", mey22)
+
+print("1.2010")
+print("2.2000")
+dec=input("choose your decade")
+
+if dec =="1":
+   print("sakamoto araws")
+
+elif dec == "2":
+     print("slam dunk")
+
+else:
+    print("kill yourself")
+  
+
+if meymey =="2" :
+    six ="sixseven"
+    print("you selected", six)
+print("1.genz")
+print("2.gen4")
+print("3.vengen")
+gen=input("choose your gens")
+
+if gen =="1":
+    mey="genz"
+    print("you selected", mey)
+
+print("1.2010")
+print("2.2000")
+dec=input("choose your decade")
+
+if dec =="1":
+   print("mayo chiki")
+
+elif dec == "2":
+     print("overflow")
+
+else:
+    print("kill yourself")
+  
+elif gen =="2":
+    mey22="gen4"
+    print("you selected", mey2)
+
+print("1.2010")
+print("2.2000")
+dec=input("choose your decade")
+
+if dec =="1":
+   print("subtopewdiepie")
+
+elif dec == "2":
+     print("just watch filthy frank or something")
+
+else:
+    print("kill yourself")
+
+elif gen =="3":
+    mey22="gen4"
+    print("you selected", mey2)
+
+print("1.2010")
+print("2.2000")
+dec=input("choose your decade")
+
+if dec =="1":
+   print("hajime no ippo")
+
+elif dec == "2":
+     print("why are you born")
+
+else:
+    print("kill yourself")
+  
+  
+if meymey =="3" :
+    ball ="balls"
+    print("you selected", ball)
+print("1.genz")
+print("2.gen4")
+print("3.vengen")
+gen=input("choose your gens")
+
+if gen =="1":
+    mey="genz"
+    print("you selected", mey)
+
+print("1.2010")
+print("2.2000")
+dec=input("choose your decade")
+
+if dec =="1":
+   print("hunter eks hunter")
+
+elif dec == "2":
+     print("your mom")
+
+else:
+    print("kill yourself")
+  
+elif gen =="2":
+    mey22="gen4"
+    print("you selected", mey2)
+
+print("1.2010")
+print("2.2000")
+dec=input("choose your decade")
+
+if dec =="1":
+   print("karakai jouzu no takagi san")
+
+elif dec == "2":
+     print("dragonballz")
+
+else:
+    print("kill yourself")
+
+elif gen =="3":
+    mey22="gen4"
+    print("you selected", mey2)
+
+print("1.2010")
+print("2.2000")
+dec=input("choose your decade")
+
+if dec =="1":
+   print("bohemian rhapsody")
+
+elif dec == "2":
+     print("slam dunk")
+
+else:
+    print("jojo bizzare adventure")
+  
+  
+  
