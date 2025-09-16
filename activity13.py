8fc1ebc50c28267d40dfd8cae5cf2ab025509af3
kurtdashielpacaigue <kurtdashielpacaigue@gmail.com>
2025-09-16 13:09:08 +0800

    Create activity13.py

---
+++ activity13.py
@@ -0,0 +1,11 @@
+gay=0
+for code in range(1,11,1):
+    print(code)
+    number = eval(input("roces is gay enter number"))
+    gay += number
+    print("\n\troces number is",gay)
+
+#gay=0
+#for code in range(11,0,-1):
+    #print(code)
+    
