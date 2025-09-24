7861c594437e272f903f5eb513857aeea77d4db7
kurtdashielpacaigue <kurtdashielpacaigue@gmail.com>
2025-09-24 14:33:32 +0800

    Create PRACTICE2..py

---
+++ PRACTICE2..py
@@ -0,0 +1,12 @@
+#name=(input("enter your name"))
+#deez=(input("enter your second name"))
+#print(f"this is your name {name.upper()+deez.upper()}")
+
+#num=eval(input("enter your number:"))
+#num2=eval(input("enter y our second number"))
+#print(f"your first num is {num} your second number is {num2}and total is {num*num2}")
+
+num=int(input("enter your number"))
+sum=1
+for num in range(num,0,-1):
+  print(f"your num is {num} and i is and this is ur final {sum * num}")
