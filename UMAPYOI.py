1f89e951415136dd2733859a397035b6a8d87ac6
kurtdashielpacaigue <kurtdashielpacaigue@gmail.com>
2025-09-26 14:57:09 +0800

    Create UMAPYOI.py

---
+++ UMAPYOI.py
@@ -0,0 +1,264 @@
+
+print("choose your character")
+print("agnes_tachyon ")
+print("t.m_opera_o")
+print("meisho_doto")
+Uma =input("pick your uma:")
+
+if Uma =="agnes_tachyon":
+    agnas= "agnes_tachyon "
+    print("you selected: ",agnas)
+    print("choose your race course")
+    print("arima_kinen")
+    print("kikuka_sho")
+    print("japanese_derby")
+    course =input("enter your race course:")
+    
+    if course =="arima_kinen":
+       arima = "arima kinen"
+       print("your selected course is",arima)
+       print("choose your style of racing")
+       print("pace")
+       print("front")
+       print("end")
+       print("late")
+       stye =input("enter your uma style:")
+     
+       if stye =="pace":
+         print("your chances of winning is 67")
+     
+       elif stye =="late":
+           print("your chances of winning is 45")
+        
+       elif stye =="end":
+           print("your chances of winning is 33")
+        
+       elif stye =="front":
+           print("your chances of winning is 89")
+        
+       else:
+           print("invalid choice")
+    elif course =="japanese_derby":
+       derby = "japanese_derby"
+       print("your selected course is: ",derby)
+       print("choose your style of racing")
+       print("pace")
+       print("front")
+       print("end")
+       print("late")
+       stye =input("enter your uma style:")
+     
+       if stye =="pace":
+         print("your chances of winning is 89")
+     
+       elif stye =="late":
+           print("your chances of winning is 68")
+        
+       elif stye =="end":
+           print("your chances of winning is 30")
+        
+       elif stye =="front":
+           print("your chances of winning is 56")
+        
+       else:
+           print("invalid choice")
+    
+    elif course =="kikuka_sho":
+       kikuka = "kikuka_sho"
+       print("your selected course is",kikuka)
+       print("choose your style of racing")
+       print("pace")
+       print("front")
+       print("end")
+       print("late")
+       stye =input("enter your uma style:")
+     
+       if stye =="pace":
+         print("your chances of winning is 56")
+     
+       elif stye =="late":
+           print("your chances of winning is 68")
+        
+       elif stye =="end":
+           print("your chances of winning is 69")
+        
+       elif stye =="front":
+           print("your chances of winning is 30")
+        
+       else:
+           print("invalid choice")
+
+elif Uma =="t.m_opera_o":
+    opera= "t.m_opera_o "
+    print("you selected:",opera)
+    print("choose your race course")
+    print("arima_kinen")
+    print("kikuka_sho")
+    print("japanese_derby")
+    course =input("enter your race course:")
+    
+    if course =="arima_kinen":
+       arima = "arima kinen"
+       print("your selected course is",arima)
+       print("choose your style of racing")
+       print("pace")
+       print("front")
+       print("end")
+       print("late")
+       stye =input("enter your uma style:")
+     
+       if stye =="pace":
+         print("your chances of winning is 46")
+     
+       elif stye =="late":
+           print("your chances of winning is 89")
+        
+       elif stye =="end":
+           print("your chances of winning is 33")
+        
+       elif stye =="front":
+           print("your chances of winning is 38")
+        
+       else:
+           print("invalid choice")
+    elif course =="japanese_derby":
+       derby = "japanese_derby"
+       print("your selected course is: ",derby)
+       print("choose your style of racing")
+       print("pace")
+       print("front")
+       print("end")
+       print("late")
+       stye =input("enter your uma style:")
+     
+       if stye =="pace":
+         print("your chances of winning is 44")
+     
+       elif stye =="late":
+           print("your chances of winning is 90")
+        
+       elif stye =="end":
+           print("your chances of winning is 28")
+        
+       elif stye =="front":
+           print("your chances of winning is 34")
+        
+       else:
+           print("invalid choice")
+    
+    elif course =="kikuka_sho":
+       kikuka = "kikuka_sho"
+       print("your selected course is",kikuka)
+       print("choose your style of racing")
+       print("pace")
+       print("front")
+       print("end")
+       print("late")
+       stye =input("enter your uma style:")
+     
+       if stye =="pace":
+         print("your chances of winning is 47")
+     
+       elif stye =="late":
+           print("your chances of winning is 91")
+        
+       elif stye =="end":
+           print("your chances of winning is 66")
+        
+       elif stye =="front":
+           print("your chances of winning is 26")
+        
+       else:
+           print("invalid choice")
+    
+elif Uma =="meisho_doto":
+    doto = "meisho_doto "
+    print("you selected:",doto)
+    print("choose your race course")
+    print("arima_kinen")
+    print("kikuka_sho")
+    print("japanese_derby")
+    course =input("enter your race course:")
+    
+    if course =="arima_kinen":
+       arima = "arima kinen"
+       print("your selected course is",arima)
+       print("choose your style of racing")
+       print("pace")
+       print("front")
+       print("end")
+       print("late")
+       stye =input("enter your uma style:")
+     
+       if stye =="pace":
+         print("your chances of winning is 96")
+     
+       elif stye =="late":
+           print("your chances of winning is 79")
+        
+       elif stye =="end":
+           print("your chances of winning is 43")
+        
+       elif stye =="front":
+           print("your chances of winning is 18")
+        
+       else:
+           print("invalid choice")
+    elif course =="japanese_derby":
+       derby = "japanese_derby"
+       print("your selected course is: ",derby)
+       print("choose your style of racing")
+       print("pace")
+       print("front")
+       print("end")
+       print("late")
+       stye =input("enter your uma style:")
+     
+       if stye =="pace":
+         print("your chances of winning is 93")
+     
+       elif stye =="late":
+           print("your chances of winning is 66")
+        
+       elif stye =="end":
+           print("your chances of winning is 43")
+        
+       elif stye =="front":
+           print("your chances of winning is 37")
+        
+       else:
+           print("invalid choice")
+    
+    elif course =="kikuka_sho":
+       kikuka = "kikuka_sho"
+       print("your selected course is",kikuka)
+       print("choose your style of racing")
+       print("pace")
+       print("front")
+       print("end")
+       print("late")
+       stye =input("enter your uma style:")
+     
+       if stye =="pace":
+         print("your chances of winning is 95")
+     
+       elif stye =="late":
+           print("your chances of winning is 61")
+        
+       elif stye =="end":
+           print("your chances of winning is 36")
+        
+       elif stye =="front":
+           print("your chances of winning is 26")
+        
+       else:
+           print("invalid choice")
+    
+
+        
+
+    
+    
+
+    
+    
