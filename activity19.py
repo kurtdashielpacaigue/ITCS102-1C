89fc232ee948f098b100c784bad6838404bc0289
kurtdashielpacaigue <kurtdashielpacaigue@gmail.com>
2025-09-29 14:28:28 +0800

    Create activity19.py

---
+++ activity19.py
@@ -0,0 +1,17 @@
+print("\t\t *",end="")
+for uma in range (1,11,1):
+    for deez in range (10,uma,-1):
+        print(" ",end=" ")
+    for y in range (1,uma,1):
+        print("*",end=" ")
+    for z in range (1,uma,1):
+        print("*",end=" ")
+    print()
+for uma in range (1,11,1):
+    for deez in range (1,uma,1):
+        print(" ",end=" ")
+    for y in range (10,uma,-1):
+        print("*",end=" ")
+    for z in range (10,uma,-1):
+        print("*",end=" ")
+    print()
