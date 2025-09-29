c86c069caf2aaf0eb703cef78aac37406ddddac1
kurtdashielpacaigue <kurtdashielpacaigue@gmail.com>
2025-09-29 14:27:49 +0800

    Create codechallenge11.py

---
+++ codechallenge11.py
@@ -0,0 +1,8 @@
+for uma in range (1,11,1):
+    for deez in range (10,uma,-1):
+        print(" ",end=" ")
+    for y in range (1,uma,1):
+        print("*",end=" ")
+    for y in range (1,uma,1):
+        print("*",end=" ")
+    print()
