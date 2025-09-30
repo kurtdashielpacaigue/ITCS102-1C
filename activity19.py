d5961d516787bc9d091b86fd002665e5324ce642
kurtdashielpacaigue <kurtdashielpacaigue@gmail.com>
2025-09-30 23:29:10 +0800

    Update activity19.py

--- activity19.py
+++ activity19.py
@@ -1,17 +1,21 @@
-print("\t\t *",end="")
-for uma in range (1,11,1):
-    for deez in range (10,uma,-1):
-        print(" ",end=" ")
-    for y in range (1,uma,1):
-        print("*",end=" ")
-    for z in range (1,uma,1):
-        print("*",end=" ")
+print("\t\t *")  
+
+for uma in range(1, 11, 1):  
+    for deez in range(10, uma, -1):
+        print(" ", end=" ")
+    for y in range(1, uma, 1):
+        print("*", end=" ")
+    for z in range(1, uma, 1):
+        print("*", end=" ")
     print()
-for uma in range (1,11,1):
-    for deez in range (1,uma,1):
-        print(" ",end=" ")
-    for y in range (10,uma,-1):
-        print("*",end=" ")
-    for z in range (10,uma,-1):
-        print("*",end=" ")
+
+for uma in range(1, 11, 1):  
+    for deez in range(1, uma, 1):
+        print(" ", end=" ")
+    for y in range(10, uma, -1):
+        print("*", end=" ")
+    for z in range(10, uma, -1):
+        print("*", end=" ")
     print()
+
+print("\t\t *") 
