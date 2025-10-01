49d0e5a4326438cb1ef08c7c4a35d870ce89522c
kurtdashielpacaigue <kurtdashielpacaigue@gmail.com>
2025-10-01 13:09:20 +0800

    Update Codechallenge12.py

--- Codechallenge12.py
+++ Codechallenge12.py
@@ -1,15 +1,15 @@
 n = 6
 
-for uma in range(1, n + 1):              # rows
-    for deez in range(n, uma, -1):       # spaces
+for uma in range(1, n + 1):              
+    for deez in range(n, uma, -1):       
         print(" ", end=" ")
 
-    for y in range(uma, 1, -1):          # left descending numbers
+    for y in range(uma, 1, -1):          
         print(y, end=" ")
 
-    print(1, end=" ")                    # middle 1
+    print(1, end=" ")                   
 
-    for z in range(2, uma + 1):          # right ascending numbers
+    for z in range(2, uma + 1):         
         print(z, end=" ")
 
     print() 
