bceb0837d2cabfd7c0a2ff3b13f981bb80d652d5
kurtdashielpacaigue <kurtdashielpacaigue@gmail.com>
2025-10-06 13:34:13 +0800

    Update activity19.py

--- activity19.py
+++ activity19.py
@@ -19,3 +19,34 @@ for uma in range(1, 11, 1):
     print()
 
 print("\t\t *") 
+
+#second more advanced triangle please input 11 i got this from an indian coder lol
+
+#n=eval(input("enter number for triangle"))
+#for i in range(1,n,1):
+   # for d in range(n,i,-1):
+        #print(" ", end=" ")
+    #for j in range(1,i,-1):
+        #print("*", end=" ")
+    #for l in range(i,1,-1):
+        #print("*", end=" ")
+
+    #print(1,end=" ")
+     
+    #for k in range(2,i+1):
+        #print("*", end=" ")
+    #print()
+
+#for i in range(n-2,0,-1):
+    #for d in range(n,i,-1):
+        #print(" ", end=" ")
+    #for j in range(1,i,-1):
+        #print("*", end=" ")
+    #for l in range(i,1,-1):
+        #print("*", end=" ")
+
+    #print("*",end=" ")
+     
+    #for k in range(2,i+1):
+        #print("*", end=" ")
+    #print()
