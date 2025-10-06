da92fa4fe2ad87f55c52a2ca783025125d427f02
kurtdashielpacaigue <kurtdashielpacaigue@gmail.com>
2025-10-06 14:12:54 +0800

    Update INDIANSCODE.py

--- INDIANSCODE.py
+++ INDIANSCODE.py
@@ -1,24 +1,28 @@
-n= 8
+n=eval(input("enter number for diamond"))
 for i in range(1,n,1):
-    for b in range(n,i,-1):
+    for d in range(n,i,-1):
         print(" ", end=" ")
-    for h in range(i,1,-1):
-        print(h, end=" ")
-    
-    print(1,end=" ")
-    
-    for j in range(2,i+1):
+    for j in range(1,i,-1):
         print(j, end=" ")
+    for l in range(i,1,-1):
+        print(l, end=" ")
+
+    print(1,end=" ")
+     
+    for k in range(2,i+1):
+        print(k, end=" ")
     print()
-    
+
 for i in range(n-2,0,-1):
-    for b in range(n,i,-1):
+    for d in range(n,i,-1):
         print(" ", end=" ")
-    for h in range(i,1,-1):
-        print(h, end=" ")
-    
-    print(1,end=" ")
-    
-    for j in range(2,i+1):
+    for j in range(1,i,-1):
         print(j, end=" ")
+    for l in range(i,1,-1):
+        print(l, end=" ")
+
+    print(1,end=" ")
+     
+    for k in range(2,i+1):
+        print(k, end=" ")
     print()
