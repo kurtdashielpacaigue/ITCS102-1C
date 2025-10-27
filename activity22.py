7335e218cb33f0af40d1a5cee37e99dfa9507fd5
kurtdashielpacaigue <kurtdashielpacaigue@gmail.com>
2025-10-27 13:35:09 +0800

    Create activity22.py

---
+++ activity22.py
@@ -0,0 +1,15 @@
+months=['jan','feb','mar','apr','may','jun','jul']
+
+print(months)
+
+months.reverse()
+print(months)
+
+fullname= 'kurt dashiel pacaigue'
+
+newlist= list(fullname)
+newlist.reverse()
+print(newlist)
+
+newlist.sort()
+print(newlist)
