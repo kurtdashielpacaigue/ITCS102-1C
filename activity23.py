df224d40a7175aa32e5410e07a55049c3a4a6336
kurtdashielpacaigue <kurtdashielpacaigue@gmail.com>
2025-11-12 13:57:55 +0800

    Update activity23.py

--- activity23.py
+++ activity23.py
@@ -1,6 +1,13 @@
+from activity23_1 import GreetWithName,FunctionWithReturn,Greetperson
+
 def Function():
     print("umamusume")
     print("agnes tachyon")
     print("4 time world champion")
 
-Function()  
+Function() 
+GreetWithName('lerbon')
+Greetperson('agnes','kyoto','67')
+
+print(f"i want to get the summation of{FunctionWithReturn(9)}")
+print(f"i want to get the summation of{FunctionWithReturn(11)+25}")
