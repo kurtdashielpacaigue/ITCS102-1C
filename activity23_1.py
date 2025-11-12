d5cc6cac49b4c8e697346ec606df828052e33c1f
kurtdashielpacaigue <kurtdashielpacaigue@gmail.com>
2025-11-12 13:57:27 +0800

    Create activity23_1.py

---
+++ activity23_1.py
@@ -0,0 +1,12 @@
+def GreetWithName(name):
+    print(f"hi{name} hope your having a great day")
+
+def Greetperson(name,loc,age):
+    print(f"hi{name}")
+
+def FunctionWithReturn(number):
+    print(f"this function calculates the summation from 1 to {number}")
+    sum=0
+    for x in range(1,number +1,1):
+        sum += x
+    return sum
