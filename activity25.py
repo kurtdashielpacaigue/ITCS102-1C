c9b73c979497b6c90b8205b946fada71a4638a81
kurtdashielpacaigue <kurtdashielpacaigue@gmail.com>
2025-11-17 14:10:28 +0800

    Create activity25.py

---
+++ activity25.py
@@ -0,0 +1,12 @@
+programming_language =['java,','rice','c','c++','perl']
+
+print(programming_language[-1])
+
+#dictionary
+
+empty_dictionary={}
+
+p1_dictionary={'genshinstuff':'buildingcharacter','jean':
+               'diluc','venti':'hutao','furina':'zhongli'}
+
+print(p1_dictionary['furina','hutao'])
