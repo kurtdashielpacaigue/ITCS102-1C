b9ce1a96a8427de4ae4a9860b535944b7e9d8946
kurtdashielpacaigue <kurtdashielpacaigue@gmail.com>
2025-11-25 13:46:58 +0800

    Create WORKINPROGRESS.py

---
+++ WORKINPROGRESS.py
@@ -0,0 +1,48 @@
+import os
+import json
+os.system('cls')
+print('DLL STUDENT INFORMATION SYSTEM')
+print('+++++++++++++++++')
+stud_record ={}
+while True:
+    print("select from the following options")
+    print("A - PRINT FUNCTION")
+    print("B - INPUT,INT,EVAL FUNCTION")
+    print("C - IF, ELSE,ELIF,FUNCTION")
+    print("D - LOOP FUNCTION")
+    print("E - WHILE TRUE FUNCTION")
+    print("F - LIST FUNCTION")
+    print("G - DEF FUNCTION")
+    print("H - EXIT SYSTEM")
+
+    choice = input("Input your choice --->").lower().strip()
+
+    if choice == 'a':
+        
+        continue
+    elif choice == 'b':
+      
+            
+        continue
+    elif choice == 'd':
+        
+        continue
+    elif choice == 'e':
+       
+            
+        continue
+    elif choice == 'f':
+       
+        continue
+
+    elif choice == 'g':
+        
+        continue
+
+
+    elif choice == 'h':
+        print("System Exit")
+        continue
+    else:
+        print("Invalid Choice, Re-enter code")
+        continue
