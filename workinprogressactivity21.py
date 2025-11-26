2eb768a5b3128cf8bb752e8e4a2a19e7ad6e3865
kurtdashielpacaigue <kurtdashielpacaigue@gmail.com>
2025-11-26 12:58:09 +0800

    Update workinprogressactivity21.py

--- workinprogressactivity21.py
+++ workinprogressactivity21.py
@@ -1,97 +1,119 @@
 import os
-import json
 
-os.system('cls' if os.name == 'nt' else 'clear')
+# -----------------------------
+# A - PRINT FUNCTION
+# -----------------------------
+def print_info():
+    print("\n--- INFORMATION ABOUT PRINT FUNCTION ---")
+    print("The print() function displays text on the screen.")
+    print("Example: print('Hello')")
 
-print("DLL STUDENT INFORMATION SYSTEM")
-print("++++++++++++++++++++++++++++++")
+def print_run():
+    print("\n--- RUNNING PRINT FUNCTION EXAMPLE ---")
+    print("Hello! This is the print() function running.")
 
+
+# -----------------------------
+# B - INPUT FUNCTION
+# -----------------------------
+def input_info():
+    print("\n--- INFORMATION ABOUT INPUT FUNCTION ---")
+    print("The input() function lets the user type values.")
+    print("Example: name = input('Enter name: ')")
+
+def input_run():
+    print("\n--- RUNNING INPUT FUNCTION EXAMPLE ---")
+    name = input("Enter your name: ")
+    print("Hello,", name)
+
+
+# -----------------------------
+# C - IF/ELSE FUNCTION
+# -----------------------------
+def ifelse_info():
+    print("\n--- INFORMATION ABOUT IF/ELSE ---")
+    print("If/Else is used for decision making.")
+    print("Example: if x > 10: print('big')")
+
+def ifelse_run():
+    print("\n--- RUNNING IF/ELSE EXAMPLE ---")
+    x = int(input("Enter a number: "))
+    if x > 10:
+        print("The number is big.")
+    else:
+        print("The number is small.")
+
+
+# -----------------------------
+# D - LOOP FUNCTION
+# -----------------------------
+def loop_info():
+    print("\n--- INFORMATION ABOUT LOOPS ---")
+    print("Loops repeat code many times.")
+    print("Example: for i in range(5): print(i)")
+
+def loop_run():
+    print("\n--- RUNNING LOOP EXAMPLE ---")
+    for i in range(1, 6):
+        print("Loop:", i)
+
+
+# -----------------------------
+# SUB-MENU HANDLER
+# -----------------------------
+def submenu(info_func, run_func, title):
+    print(f"\nYou selected: {title}")
+    print("1 - Information about this code")
+    print("2 - Run this code")
+
+    sub = input("Choose ---> ").strip()
+
+    if sub == '1':
+        info_func()
+    elif sub == '2':
+        run_func()
+    else:
+        print("Invalid selection.")
+
+    input("\nPress Enter to continue...")
+
+
+# -----------------------------
+# MAIN PROGRAM LOOP
+# -----------------------------
 while True:
-    print("\nSelect from the following options")
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    print("DLL STUDENT INFORMATION SYSTEM")
+    print("++++++++++++++++++++++++++++++\n")
     print("A - PRINT FUNCTION")
     print("B - INPUT FUNCTION")
-    print("C - IF/ELSE FUNCTION")
+    print("C - IF, ELSE FUNCTION")
     print("D - LOOP FUNCTION")
     print("E - EXIT SYSTEM")
 
     choice = input("\nInput your choice ---> ").lower().strip()
 
-    # ----------------------
-    # PRINT FUNCTION (A)
-    # ----------------------
     if choice == 'a':
-        print("\nYou selected PRINT FUNCTION")
-        print("1 - Information about the code")
-        print("2 - Run the code")
-
-        sub = input("Choose ---> ").strip()
-
-        if sub == '1':
-            print("\n--- INFORMATION ABOUT PRINT FUNCTION ---")
-            print("The print() function displays text or variables on the screen.")
-            print("Example: print('Hello')")
-        elif sub == '2':
-            print("\n--- RUNNING PRINT FUNCTION EXAMPLE ---")
-            print("Hello! This is the print function running.")
-        else:
-            print("Invalid selection.")
-
-        input("\nPress Enter to continue...")
+        submenu(print_info, print_run, "PRINT FUNCTION")
         continue
 
-    # ----------------------
-    # INPUT FUNCTION (B)
-    # ----------------------
     elif choice == 'b':
-        print("\nYou selected INPUT FUNCTION")
-        print("1 - Information about the code")
-        print("2 - Run the code")
-
-        sub = input("Choose ---> ").strip()
-
-        if sub == '1':
-            print("\n--- INFORMATION ABOUT INPUT FUNCTION ---")
-            print("The input() function lets the user enter information.")
-            print("Example: name = input('Enter your name: ')")
-        elif sub == '2':
-            print("\n--- RUNNING INPUT FUNCTION EXAMPLE ---")
-            name = input("Enter your name: ")
-            print("Hello,", name)
-        else:
-            print("Invalid selection.")
-
-        input("\nPress Enter to continue...")
+        submenu(input_info, input_run, "INPUT FUNCTION")
         continue
 
-    # ----------------------
-    # IF/ELSE FUNCTION (C)
-    # ----------------------
     elif choice == 'c':
-        print("\nYou selected IF/ELSE FUNCTION")
-        print("1 - Information about the code")
-        print("2 - Run the code")
-
-        sub = input("Choose ---> ").strip()
-
-        if sub == '1':
-            print("\n--- INFORMATION ABOUT IF/ELSE ---")
-            print("If/Else is used for decisions in Python.")
-            print("Example: if x > 10: print('Big')")
-        elif sub == '2':
-            print("\n--- RUNNING IF/ELSE EXAMPLE ---")
-            x = int(input("Enter a number: "))
-            if x > 10:
-                print("Number is big.")
-            else:
-                print("Number is small.")
-        else:
-            print("Invalid selection.")
-
-        input("\nPress Enter to continue...")
+        submenu(ifelse_info, ifelse_run, "IF/ELSE FUNCTION")
         continue
 
-    # ----------------------
-    # LOOP FUNCTION (D)
-    # ----------------------
     elif choice == 'd':
-        print("\nYou selected LOOP FUNCTION"
+        submenu(loop_info, loop_run, "LOOP FUNCTION")
+        continue
+
+    elif choice == 'e':
+        print("\nExiting the system...")
+        break
+
+    else:
+        print("Invalid input. Try again.")
+        input("Press Enter to continue...")
