201de0feae227f19a22c06215337497dc99d36e1
kurtdashielpacaigue <kurtdashielpacaigue@gmail.com>
2026-01-16 23:50:09 +0800

    Add Genshin characters random selection script

---
+++ arraystraining.py
@@ -0,0 +1,15 @@
+import random
+genshin_characters = [
+    "Aether", "Lumine",
+    "Amber", "Kaeya", "Lisa", "Jean", "Diluc",
+    "Venti", "Xiangling", "Xingqiu", "Bennett", "Razor",
+    "Zhongli", "Ningguang", "Keqing", "Ganyu", "Xiao", "Hu Tao",
+    "Tartaglia", "Shenhe", "Yelan",
+    "Raiden Shogun", "Kujou Sara", "Kamisato Ayaka", "Kamisato Ayato",
+    "Yoimiya", "Sayu", "Thoma",
+    "Nahida", "Nilou", "Cyno", "Alhaitham", "Dehya",
+    "Furina", "Neuvillette", "Lyney", "Lynette", "Wriothesley"
+]
+hoyo=random.randint(0,38)
+print(f'this is the number of your character:{hoyo}')
+print("Your Character is:"+genshin_characters[hoyo])
