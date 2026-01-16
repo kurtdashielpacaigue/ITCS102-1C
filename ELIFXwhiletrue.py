04cf411586afc6fd3f137a5d63420e9c7b08ff2e
kurtdashielpacaigue <kurtdashielpacaigue@gmail.com>
2026-01-17 00:51:39 +0800

    Create ELIFXwhiletrue.py

---
+++ ELIFXwhiletrue.py
@@ -0,0 +1,42 @@
+import os
+while True:
+
+    print("what your character tells about you")
+    print('your optional characters:' \
+    '\nhutao,  furina, diluc,  MaxVerstappen' \
+    '\nineffa,venti')
+    a =input("\nenter your desired character:")
+    if a =='hutao':
+         os.system('cls')
+         print(f'you selected {a}:you are goated live life until death  or buy a coffin')
+         input()
+         continue
+    elif a  =='furina':
+         os.system('cls')
+         print(f'you selected {a}:be grateful in the precense of your god')
+         input()
+         continue
+    elif a  =='diluc':
+         os.system('cls')
+         print(f'you selected {a}:if you feel like a failure then be better change\n will not happen by itself')
+         continue
+    elif a  =='MaxVerstappen':
+         os.system('cls')
+         print(f'you selected {a}:always try to learn from your mistakes and never give up')
+         input()
+         continue
+    elif a  =='ineffa':
+         os.system('cls')
+         print(f'you selected {a}:dont trust chatgpt she a bitch')
+         input()
+         continue
+    elif a  =='venti':
+         os.system('cls')
+         print(f'you selected {a}:shot munabago ma stress sa 2nd sem')
+         input()
+         continue
+    else:
+         print("Sorry Its not included in the choices")
+         break
+        
+    
